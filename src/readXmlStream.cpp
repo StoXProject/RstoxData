4f457cc5afc1847a3a06ac390fce5da09738fc8f
Ibrahim Umar <ibrahim.umar@uit.no>
2019-12-17 22:13:48 +0100

    XML stream read speed improvements

--- src/readXmlStream.cpp
+++ src/readXmlStream.cpp
@@ -14,6 +14,30 @@
 #define STRICT_R_HEADERS
 #include "Rcpp.h"
 
+// -- Trim functions (source: https://stackoverflow.com/a/25385766)
+// --
+const char* ws = " \t\n\r\f\v";
+
+// trim from end of string (right)
+inline std::string& rtrim(std::string& s, const char* t = ws)
+{
+    s.erase(s.find_last_not_of(t) + 1);
+    return s;
+}
+
+// trim from beginning of string (left)
+inline std::string& ltrim(std::string& s, const char* t = ws)
+{
+    s.erase(0, s.find_first_not_of(t));
+    return s;
+}
+
+// trim from both ends of string (right then left)
+inline std::string& trim(std::string& s, const char* t = ws)
+{
+    return ltrim(rtrim(s, t), t);
+}
+// --
 
 class persistentData {
 private:
@@ -130,12 +154,15 @@ public:
 
 static void sDataHandler(const XML_Char *data, size_t len, void *userData)
 {
-	if(len > 0 && data[0] != '\0') {
+	if(len) {
+		// Test string
+		std::string teststr(data, len);
+		trim(teststr);
 
-		// Put data inside string
-		std::string strdata(data, len);
+		if(teststr.length()) {
+			// Put data inside string
+			std::string strdata(data, len);
 
-		if(strdata.size() > 0) {
 			// Parse data
 			passingData* pD = (passingData*) userData;
 
