99ab665283cda2eb67ebb6a86bdd5eb7e506c353
Ibrahim Umar <ibrahim.umar@uit.no>
2019-12-19 17:13:56 +0100

    Add tests for StoxBiotic and StoxAcoustic functions

---
+++ tests/testthat/test-StoxBiotic.R
@@ -0,0 +1,16 @@
+# StoxBiotic
+example <- system.file("testresources","biotic_v3_example.xml", package="RstoxData")
+
+context("test-StoxBiotic: using DOM biotic")
+defaultParseBiotic <- readXmlFile(example, stream = F)
+sb1 <- StoxBiotic(list(defaultParseBiotic))
+expect_equal(nrow(sb1$Haul), 2)
+
+context("test-StoxBiotic: using stream parse biotic")
+streamParseBiotic <- readXmlFile(example, stream = T)
+sb2 <- StoxBiotic(list(streamParseBiotic))
+expect_equal(nrow(sb2$Haul), 2)
+
+context("test-StoxBiotic: all.equal")
+expect_true(all.equal(sb1, sb2))
+
