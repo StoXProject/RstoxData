8b05f18eab2422a4259d2300af25263283369451
sindre vatnehol <sindre.vatnehol@imr.no>
2020-03-24 14:22:56 +0100

    update on export (not finnished)

--- R/StoxExport.R
+++ R/StoxExport.R
@@ -4,21 +4,53 @@ write2ICESacoustic_CSV <- function(Acoustic,save=T){
   for(aco in Acoustic){
     if(aco$metadata$useXsd=='icesAcoustic'){
       
-      hl <- data.frame(aco$Instrument)
+      hl<-c()
+      hl$Instrument <- 'Instrument'
+      hl$Header <- 'Record'
+      tmp <- data.frame(aco$Instrument)
+      names(tmp)<-paste0('Instrument',names(tmp))
+      hl <- cbind(hl,tmp)
       HInst <- format(hl, trim=TRUE, width=0)
       
-      hl <- data.frame(aco$Calibration)
+      hl<-c()
+      hl$Calibration <- 'Calibration'
+      hl$Header <- 'Record'
+      tmp <- data.frame(aco$Calibration)
+      names(tmp)<-paste0('Calibration',names(tmp))
+      hl <- cbind(hl,tmp)
       HCal <- format(hl, trim=TRUE, width=0)
       
-      hl <- data.frame(aco$DataAcquisition)
+      hl<-c()
+      hl$DataAcquisition <- 'DataAcquisition'
+      hl$Header <- 'Record'
+      tmp <- data.frame(aco$DataAcquisition)
+      names(tmp)<-paste0('DataAcquisition',names(tmp))
+      hl <- cbind(hl,tmp)
       HDatA <- format(hl, trim=TRUE, width=0)
       
-      hl <- data.frame(aco$DataProcessing)
+      hl<-c()
+      hl$DataProcessing <- 'DataProcessing'
+      hl$Header <- 'Record'
+      tmp <- data.frame(aco$DataProcessing)
+      names(tmp)<-paste0('DataProcessing',names(tmp))
+      hl <- cbind(hl,tmp)
       HDatP <- format(hl, trim=TRUE, width=0)
       
-      hl <- data.frame(aco$Cruise)
+      hl<-c()
+      hl$Cruise <- 'Cruise'
+      hl$Header <- 'Record'
+      hl$CruiseSurvey <- aco$Survey[2]
+      tmp <- data.frame(aco$DataProcessing)
+      names(tmp)<-paste0('DataProcessing',names(tmp))
+      hl <- cbind(hl,tmp)
       HCru <- format(hl, trim=TRUE, width=0)
       
+      
+      hl<-c()
+      hl$Cruise <- 'Data'
+      hl$Header <- 'Record'
+      tmp <- data.frame(aco$Data)
+      names(tmp)<-paste0('DataProcessing',names(tmp))
       hl <- data.frame(aco$Data)
       HDat <- format(hl, trim=TRUE, width=0)
       
@@ -30,9 +62,13 @@ write2ICESacoustic_CSV <- function(Acoustic,save=T){
                   Data=HDat
       )
       
-      if(save==T)suppressWarnings(lapply(tmp, write.table, file=gsub('.xml','.csv',aco$metadata$file), append=TRUE, row.names=FALSE, quote=FALSE, sep=","))
+      if(save==T){
+        filename = gsub('.xml','.csv',aco$metadata$file)
+        if(file.exists(filename))file.remove(filename)
+        suppressWarnings(lapply(tmp, write.table, file=filename, append=TRUE, row.names=FALSE, quote=FALSE, sep=","))
+        }
       
-    }else{'Warning: only ices acoustic format is allowed'}
+    }else{warning('only ices acoustic format is allowed')}
 
   }
   
