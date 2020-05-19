36bcd41e9a4d0f3a1f13ed7e7aebe901ea09173c
arnejohannesholmin <arnejh@hi.no>
2020-05-19 14:04:58 +0200

    Release 0.8.8

--- R/pkgname.R
+++ R/pkgname.R
@@ -2,7 +2,7 @@
 #'
 #' Tools to fetch and manipulate various data formats for fisheries (mainly geared towards biotic and acoustic data).
 #'
-#' The RstoxData package contains functions for reading, filtering and writing biotic, acoustic and landing data as XML files. Filtering can be done by R syntax such as longitude > 10, or by pre defined functions such as inside().
+#' The RstoxData package contains functions for reading, filtering and writing biotic, acoustic and landing data as XML files. Filtering can be done by R syntax such as longitude > 10, or by pre defined functions such as inside(). On computers that return errors when trying to run the Rtools through RStudio (most institutional Windows machines), install the binary directly from https://github.com/StoXProject/RstoxData/releases. Download the newest RstoxData zip file, click the "Packages" tab -> "Install" -> "Install from:" "Package Archive File" -> "Install". If the installer does not complain, the package is installed correctly.
 #' @docType package
 #' @name RstoxData
 #'
