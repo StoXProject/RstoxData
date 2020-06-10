1a406547e6607add45f70a29d09c2b9b5a2d1557
Ibrahim Umar <ibrahim.umar@imr.no>
2020-06-10 11:49:28 +0200

    Add NMD Biotic format v3.1

--- data-raw/processBioticData.R
+++ data-raw/processBioticData.R
@@ -9,6 +9,30 @@ stoxBioticObject$convertLenRes <- list()
 stoxBioticObject$convertLen <- list()
 stoxBioticObject$convertWt <- list()
 
+# NMDBioticv3.1
+stoxBioticObject$indageHeadersList[["nmdbioticv3.1"]] <- c("missiontype", "startyear", "platform", "missionnumber", "serialnumber", "catchsampleid", "specimenid")
+
+## Format: {source variable, target keyname}
+stoxBioticObject$tableKeyList[["nmdbioticv3.1"]] <- list(
+                 list(c("cruise", "missiontype", "startyear", "platform", "missionnumber"), "CruiseKey"),
+                 list("station", "StationKey"),
+                 list("serialnumber", "HaulKey"),
+                 list(c("commonname", "catchcategory", "aphia", "scientificname"), "SpeciesCategoryKey"),
+                 list("catchpartnumber", "SampleKey"),
+                 list("specimenid", "IndividualKey"),
+                 list("preysampleid", "SubIndividualKey")
+                )
+stoxBioticObject$tableMapList[["nmdbioticv3.1"]] <- list(list("mission", "Cruise"), list("individual", "Individual"), list("prey", "SubIndividual"))
+stoxBioticObject$complexMaps[["nmdbioticv3.1"]] <- fread("stox-translate-nmdbioticv3.1.csv", stringsAsFactors=FALSE)
+## Length conversion
+stoxBioticObject$convertLenRes[["nmdbioticv3.1"]] <- function(x) {
+	z <- c(0.001, 0.005, 0.01, 0.03, 0.05, 0.0005, 0.0001, 0.0001, 0.002, 0.003, 0.02, 0.2) * 100
+	names(z) <- seq_len(12)
+	return(z[x])
+}
+stoxBioticObject$convertLen[["nmdbioticv3.1"]] <- NULL
+stoxBioticObject$convertWt[["nmdbioticv3.1"]] <- NULL
+
 # NMDBioticv3
 stoxBioticObject$indageHeadersList[["nmdbioticv3"]] <- c("missiontype", "startyear", "platform", "missionnumber", "serialnumber", "catchsampleid", "specimenid")
 
