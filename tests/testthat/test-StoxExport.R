2a4f66f699f37ad729b52ce82a77c42d1f27e195
Ibrahim Umar <ibrahim.umar@hi.no>
2020-11-23 11:56:13 +0100

    Tests: bring back some tests for StoxExport functions

--- tests/testthat/test-StoxExport.R
+++ tests/testthat/test-StoxExport.R
@@ -8,7 +8,7 @@ data <- ReadBiotic(example)
 
 data[[1]]$fishstation[, stationstartdate := stationstopdate]
 datras1 <- RstoxData::WriteICESDatras(data, Combine = FALSE)
-#datras2 <- RstoxData::WriteICESDatras(data, Combine = TRUE)
+datras2 <- RstoxData::WriteICESDatras(data, Combine = TRUE)
 expect_equal(nrow(datras1[[1]]$HH), 2)
 
 context("test-StoxExport: ICES biotic export")
@@ -17,7 +17,7 @@ data <- ReadBiotic(example)
 
 data[[1]]$fishstation[, stationstartdate := stationstopdate]
 icesbiotic1 <- RstoxData::WriteICESBiotic(data, Combine = FALSE)
-#icesbiotic2 <- RstoxData::WriteICESBiotic(data, Combine = TRUE)
+icesbiotic2 <- RstoxData::WriteICESBiotic(data, Combine = TRUE)
 expect_equal(nrow(icesbiotic1[[1]]$Haul), 2)
 
 
@@ -25,7 +25,7 @@ context("test-StoxExport: ICES acoustic export #1")
 example <- system.file("testresources", "ICES_Acoustic_1.xml", package="RstoxData")
 data <- ReadAcoustic(example)
 icesacoustic1 <- RstoxData::WriteICESAcoustic(data, Combine = FALSE)
-#icesacoustic2 <- RstoxData::WriteICESAcoustic(data, Combine = TRUE)
+icesacoustic2 <- RstoxData::WriteICESAcoustic(data, Combine = TRUE)
 
 expect_equal(nrow(icesacoustic1[[1]]$Data), 11)
 
@@ -33,6 +33,6 @@ context("test-StoxExport: ICES acoustic export #2")
 example <- system.file("testresources", "ICES_Acoustic_2.xml", package="RstoxData")
 data <- ReadAcoustic(example)
 icesacoustic1 <- RstoxData::WriteICESAcoustic(data, Combine = FALSE)
-#icesacoustic2 <- RstoxData::WriteICESAcoustic(data, Combine = TRUE)
+icesacoustic2 <- RstoxData::WriteICESAcoustic(data, Combine = TRUE)
 
 expect_equal(nrow(icesacoustic1[[1]]$Data), 12)
