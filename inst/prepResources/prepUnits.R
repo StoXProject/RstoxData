b3a238dd3f62e1cc1f389a56df83eeaa984935fe
edvinf <edvin.fuglebakk@gmail.com>
2021-12-17 17:02:06 +0100

    Suggestion for format for Stox unit definitions and conversion function

--- inst/prepResources/prepUnits.R
+++ inst/prepResources/prepUnits.R
@@ -2,39 +2,26 @@
 # Run this script to regnerate the RstoxData resource 'StoxUnits'
 # If the format is extended, update the documentation in R/Stox-Units-datadoc.R
 #
-# Units should be considered defined by quantity, id, and conversion. 
-# Changing any of these for a unit breaches backwards compatibility.
-# symbol, shortname and name are auxiliary information. 
-# These could be changed or clarified, but must be kept unique for a given quantity.
 
-StoxUnits <- data.table::data.table(id=character(),
-                                    quantity=character(),
+StoxUnits <- data.table::data.table(quantity=character(),
                                     symbol=character(),
-                                    shortname=character(),
                                     name=character(),
                                     conversion=numeric())
 
-StoxUnits <- rbind(StoxUnits, list("mass-g", "mass", "g", "g", "gram", 1e-3))
-StoxUnits <- rbind(StoxUnits, list("mass-kg", "mass", "kg", "kg", "kilogram", 1))
-StoxUnits <- rbind(StoxUnits, list("mass-ton", "mass", "t", "ton", "metric ton", 1e3))
-StoxUnits <- rbind(StoxUnits, list("mass-kt", "mass", "kt", "kiloton", "kiloton", 1e6))
-StoxUnits <- rbind(StoxUnits, list("length-mm", "length", "mm", "mm", "millimeter", 1e-3))
-StoxUnits <- rbind(StoxUnits, list("length-cm", "length", "cm","cm", "centimeter", 1e-2))
-StoxUnits <- rbind(StoxUnits, list("length-m", "length", "m", "m", "meter", 1))
-StoxUnits <- rbind(StoxUnits, list("length-km", "length", "km", "km", "kilometer", 1e3))
-StoxUnits <- rbind(StoxUnits, list("length-nmi", "length", "M", "nmi", "nautical mile", 1852)) #M is hydrographic standard
-StoxUnits <- rbind(StoxUnits, list("cardinality-N", "cardinality", "N", "individuals", "individuals", 1))
-StoxUnits <- rbind(StoxUnits, list("cardinality-kN", "cardinality", "kN", "10^3 individuals", "thousand individuals", 1e3))
-StoxUnits <- rbind(StoxUnits, list("cardinality-MN", "cardinality", "MN", "10^6 individuals", "million individuals", 1e6))
-StoxUnits <- rbind(StoxUnits, list("fraction-decimal", "fraction", "0.", "fraction", "decimal", 1))
-StoxUnits <- rbind(StoxUnits, list("fraction-percent", "fraction", "%", "%", "percent", 1e-2))
-# treat age separate from time, as it is not a fixed ratio to actual time units
-StoxUnits <- rbind(StoxUnits, list("age-year", "age", "y", "year", "year", 1))
-StoxUnits <- rbind(StoxUnits, list("time-s", "time", "s", "second", "second", 1))
+StoxUnits <- rbind(StoxUnits, list("mass", "g", "gram", 1e-3))
+StoxUnits <- rbind(StoxUnits, list("mass", "kg", "kilogram", 1))
+StoxUnits <- rbind(StoxUnits, list("mass", "t", "metric ton", 1e3))
+StoxUnits <- rbind(StoxUnits, list("mass", "kt", "kiloton", 1e6))
+StoxUnits <- rbind(StoxUnits, list("length", "mm", "millimeter", 1e-3))
+StoxUnits <- rbind(StoxUnits, list("length", "cm", "centimeter", 1e-2))
+StoxUnits <- rbind(StoxUnits, list("length", "m", "meter", 1))
+StoxUnits <- rbind(StoxUnits, list("length", "km", "kilometer", 1e3))
+StoxUnits <- rbind(StoxUnits, list("length", "nmi", "nautical mile", 1852))
+StoxUnits <- rbind(StoxUnits, list("cardinality", "i", "individuals", 1))
+StoxUnits <- rbind(StoxUnits, list("cardinality", "ki", "thousand individuals", 1e3))
+StoxUnits <- rbind(StoxUnits, list("cardinality", "Mi", "million individuals", 1e6))
 
-stopifnot(!any(duplicated(paste(StoxUnits$id))))
-
-StoxUnits <- StoxUnits[,c("id", "conversion", "quantity", "symbol", "shortname", "name")]
+stopifnot(!any(duplicated(StoxUnits$symbol)))
 
 usethis::use_data(StoxUnits, overwrite = T)
 
