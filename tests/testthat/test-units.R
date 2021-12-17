2a6c10cc695dc37eb30793dac5a6587fb100887a
edvinf <edvin.fuglebakk@gmail.com>
2021-12-17 17:02:06 +0100

    Suggestion for format for Stox unit definitions and conversion function

---
+++ tests/testthat/test-units.R
@@ -0,0 +1,14 @@
+context("Test unit conversion")
+
+m <- convertUnits(1, "km", "m")
+expect_equal(m,1000)
+
+m <- convertUnits(2.5, "g", "kt")
+expect_equal(m,2.5e-9)
+
+expect_error(convertUnits(2.5, "nmi", "kt"), "nmi and kt are of different quantities.")
+expect_error(convertUnits(2.5, "kts", "kt"), "Symbol kts not found in 'conversionTable'")
+
+m <- convertUnits(c(1,2), "km", "nmi")
+
+expect_equal(m,c(1000/1852, 2000/1852))
