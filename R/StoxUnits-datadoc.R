9d7accc2b7bf3f854302d2256598c4ef6e4d2bc5
edvinf <edvin.fuglebakk@gmail.com>
2021-12-17 17:02:06 +0100

    Suggestion for format for Stox unit definitions and conversion function

--- R/StoxUnits-datadoc.R
+++ R/StoxUnits-datadoc.R
@@ -11,7 +11,7 @@
 #'   \item{quantity}{The quantity the units apply to. Units can be converted to other units of the same quantity}
 #'   \item{symbol}{Symbol or shorthand notation for the unit}
 #'   \item{name}{Name of the unit}
-#'   \item{conversion}{Conversion factor. All units of the same quantity are expressed relative to common base unit whos value for conversion is 1.}
+#'   \item{conversion}{Conversion factor. All units of the same quantity are expressed relative to common reference whos value for conversion is 1.}
 #'  }
 #'
 #' @keywords datasets
