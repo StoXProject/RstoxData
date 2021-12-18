8ddd7529712389c1dee7ba4f764ff10d17e5e82e
edvinf <edvin.fuglebakk@gmail.com>
2021-12-18 01:46:53 +0100

    Added unit for fractions

--- tests/testthat/test-units.R
+++ tests/testthat/test-units.R
@@ -12,3 +12,8 @@ expect_error(convertUnits(2.5, "kts", "kt"), "Symbol kts not found in 'conversio
 m <- convertUnits(c(1,2), "km", "nmi")
 
 expect_equal(m,c(1000/1852, 2000/1852))
+
+m1 <- convertUnits(20, "%", "0.")
+expect_equal(m1,.2)
+m2 <- convertUnits(.20, "0.", "%")
+expect_equal(m2,20)
