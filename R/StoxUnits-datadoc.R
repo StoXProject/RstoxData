fdf91165e679ed531bf63d3f0074f354e063f724
edvinf <edvin.fuglebakk@gmail.com>
2021-12-21 14:32:25 +0100

    Changed structure to make symbol, name and shortname independent of unit identification

--- R/StoxUnits-datadoc.R
+++ R/StoxUnits-datadoc.R
@@ -8,11 +8,17 @@
 #'
 #' @format A \code{\link[data.table]{data.table}} with the following columns:
 #'  \describe{
+#'   \item{id}{unique identifier for unit}
 #'   \item{quantity}{The quantity the units apply to. Units can be converted to other units of the same quantity}
-#'   \item{symbol}{Symbol or shorthand notation for the unit}
+#'   \item{symbol}{Symbol or shorthand notation for the unit. Unique within quantity}
+#'   \item{shortname}{Short name of the unit, intended for plotting and menu options}
 #'   \item{name}{Name of the unit}
 #'   \item{conversion}{Conversion factor. All units of the same quantity are expressed relative to common base unit whos value for conversion is 1.}
 #'  }
+#'  
+#'  The 'id', 'quantity', and 'conversion' defines a unit. 
+#'  'symbol', shortname' and 'name' are descriptions, which may be exchanged without affecting RstoxData functions that make use of this table.
+#'  'symbol', shortname' and 'name' should always be unique for a given quantity
 #'
 #' @keywords datasets
 #'
