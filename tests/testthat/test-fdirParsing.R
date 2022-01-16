a0636b45fd66ed4bacbefefb1d9362eb8df2aa34
edvinf <edvin.fuglebakk@gmail.com>
2022-01-16 03:14:40 +0100

    Added function for converting lss data to xml-landings

--- tests/testthat/test-fdirParsing.R
+++ tests/testthat/test-fdirParsing.R
@@ -15,6 +15,18 @@ expect_equal(nrow(data),9)
 suppressMessages(nonstrictdata <- readLssFile(system.file("testresources","landings_trimmed_2018.lss", package="RstoxData"), strict=F))
 expect_equal(nrow(data), nrow(nonstrictdata))
 expect_equal(ncol(data), ncol(nonstrictdata))
+expect_true(all(names(data) == names(nonstrictdata)))
+
+context("readLssFile: wrong headers")
+expect_warning(wh <- readLssFile(system.file("testresources","landings_trimmed_2018_alt_header.lss", package="RstoxData"), strict=T))
+expect_equal(nrow(data), nrow(wh))
+expect_equal(ncol(data), ncol(wh))
+expect_true(all(names(data) == names(wh))) #make sure names are the same even if they are not in the data file
+
+context("Test convertLandings")
+landings <- convertToLandingData(data)
+expect_true(RstoxData::is.LandingData(landings))
+expect_equal(sum(landings$ConvertedData$Produkt$Rundvekt), sum(data$Rundvekt))
 
 context("readErsFile: normal run")
 data <- readErsFile(system.file("testresources","logbooks_trimmed_2015.psv", package="RstoxData"))
